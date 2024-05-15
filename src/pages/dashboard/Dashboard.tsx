d97c7dc25787ea117e08bfad6b6568511f3a4b67
gsousaaa <glaucoezequiel94@gmail.com>
2024-05-15 00:33:09 -0300

    Criando a página do Dashboard.

---
+++ src/pages/dashboard/Dashboard.tsx
@@ -0,0 +1,21 @@
+import { Button } from '../../components/common/Button'
+import { BoxDashboard } from '../../components/layout/BoxDashboard'
+
+const Dashboard = () => {
+    return(
+        <section >
+            <div className='pt-3 flex justify-end pr-10'> 
+                <Button type='button'>Logout</Button>
+            </div>
+
+        <div className="flex w-full h-screen" >
+            <div className="bg-gray-100 w-full flex items-center justify-center">
+                    <BoxDashboard/>
+            </div>
+        </div>
+    </section>
+    )
+}
+
+
+export default Dashboard
