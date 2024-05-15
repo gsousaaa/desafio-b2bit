aae9c7c5b5c6c645d8477401ebebb2fafa897f44
gsousaaa <glaucoezequiel94@gmail.com>
2024-05-14 21:51:47 -0300

    Alteração na estilização do componente Input

--- src/components/common/Input.tsx
+++ src/components/common/Input.tsx
@@ -1,15 +1,15 @@
 interface InputProps  {
     type: string,
     placeholder: string,
-    value?: string,
-    onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void,
-    name?: string,
+    value: string,
+    onChange: (e: React.ChangeEvent<HTMLInputElement>) => void,
+    name: string,
 
 }
 
 export const Input: React.FC<InputProps> = ({type, placeholder, value, onChange, name}) => {
     return (
-            <input className=' bg-gray-200 text-black w-full border-2 border-gray-100 rounded-xl p-4 mt-1 bg-transparent'type={type} placeholder={placeholder} value={value} onChange={onChange} name={name}/>
+            <input className='text-sm text-black w-full border-2 border-gray-100 rounded-xl p-4 mt-1 bg-gray-100'type={type} placeholder={placeholder} value={value} onChange={onChange} name={name}/>
     )
 }
 
