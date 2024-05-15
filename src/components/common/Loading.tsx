a3c062e0b13151acda1db59fceada3c764bc7d43
gsousaaa <glaucoezequiel94@gmail.com>
2024-05-15 11:21:07 -0300

    Implementando componente de loading

---
+++ src/components/common/Loading.tsx
@@ -0,0 +1,6 @@
+
+export const Loading = () => {
+    return (
+        <div className="h-12 w-12 border-4 border-l-gray-200 border-r-gray-200 border-b-gray-200 border-t-blue-950 animate-spin ease-linear rounded-full"></div>
+    )
+}
