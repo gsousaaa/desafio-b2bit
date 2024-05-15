bcaf9b2bd109416875cdf04700dc1914e93a2705
gsousaaa <glaucoezequiel94@gmail.com>
2024-05-15 16:52:44 -0300

    Implementando o react-toastify para exibir os erros na página de login

--- src/pages/auth/LoginPage.tsx
+++ src/pages/auth/LoginPage.tsx
@@ -2,22 +2,35 @@
 import { FormLogin } from "../../components/layout/FormLogin"
 import { useNavigate } from 'react-router-dom';
 import { Credentials, loginUser } from "../../services/api"
+import { toast } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
 
 const Login = () => {
     const navigate = useNavigate()
 
-    const handleLogin = async ({ email, password }: Credentials) => {
+    const handleLogin = async ( { email, password }: Credentials) => {
+
+        if(email == '' || password == '') {
+          toast.error('Preencha todos os campos!')
+          return
+        }
+
         try {
             let loginData = await loginUser({email, password})
             if(loginData && loginData.tokens.access) {
                 navigate('/dashboard')
-                
+
                 setTimeout(() => {
                   localStorage.removeItem('accessToken')
                 }, 3600000)
             }
-        } catch(e) {
-          console.log('Erro ao fazer login')
+        } catch(e: any) {
+            if(e.response) {
+              let responseData = e.response.data
+              toast.error(responseData.detail);
+            } else {
+              toast.error('Erro ao fazer login')
+            }
         }
     }
  
