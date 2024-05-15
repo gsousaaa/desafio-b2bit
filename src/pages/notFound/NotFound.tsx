9e31b523cbbb66e9a6ac15e1d4ffb6838b2b9619
gsousaaa <glaucoezequiel94@gmail.com>
2024-05-14 21:52:21 -0300

    Declarando a página notFound (fazer estilização)

---
+++ src/pages/notFound/NotFound.tsx
@@ -0,0 +1,13 @@
+
+const NotFound = () => {
+    return (
+        <section>
+            <h2>Página não encontrada, voltar para home</h2>
+            <a href="/">Home</a>
+        </section>
+    )
+}
+
+
+export default NotFound
+
