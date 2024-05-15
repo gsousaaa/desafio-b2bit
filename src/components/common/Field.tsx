3bfaf09a893c8ade215dcf872a04374ff542d117
gsousaaa <glaucoezequiel94@gmail.com>
2024-05-14 21:10:45 -0300

    Declarando o component Field, para exibir as informações dos usuários.

---
+++ src/components/common/Field.tsx
@@ -0,0 +1,17 @@
+
+interface FieldProps {
+    type: string,
+    value: string,
+    nameTitle: string,
+}
+
+export const Field: React.FC<FieldProps> = ({ type, value, nameTitle }) => {
+
+    return (
+        <>
+            <label>Your <b>{nameTitle}</b></label>
+            <input  className="outline-none cursor-auto bg-slate-100 text-black w-full border-2 border-gray-100 rounded-xl p-4 mt-1 bg-gray-100 text-sm" type={type} value={value} readOnly />
+        </>
+
+    )
+}   
