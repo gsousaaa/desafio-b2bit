b2da885a06a63538d502d9a67eb14814d98766b7
gsousaaa <glaucoezequiel94@gmail.com>
2024-05-15 11:54:06 -0300

    Umplementando lógica para o logout.

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -1,11 +1,27 @@
 import { Button } from '../../components/common/Button'
 import { BoxDashboard } from '../../components/layout/BoxDashboard'
+import { useNavigate } from 'react-router-dom'
 
 const Dashboard = () => {
+    const navigate = useNavigate()
+
+    const handleLogout = () => {
+        try {
+            const token = localStorage.getItem('accessToken')
+            if(token) {
+                localStorage.removeItem('accessToken')
+                navigate('/')
+            }
+            
+        } catch(e) {
+            console.log("Erro ao fazer logout")
+        }
+    }
+
     return(
         <section >
             <div className='pt-3 flex justify-end pr-10 pb-3'> 
-                <Button type='button'>Logout</Button>
+                <Button onClick={handleLogout} type='button'>Logout</Button>
             </div>
 
         <div className="flex w-full h-screen" >
