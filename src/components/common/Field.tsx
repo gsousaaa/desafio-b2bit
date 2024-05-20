1f976f943b7f0458389bf9292a50c177b1e93300
gsousaaa <glaucoezequiel94@gmail.com>
2024-05-20 12:22:31 -0300

    Alterando font-size no componente Field.

--- src/components/common/Field.tsx
+++ src/components/common/Field.tsx
@@ -9,7 +9,7 @@ export const Field: React.FC<FieldProps> = ({ type, value, nameTitle }) => {
 
     return (
         <>
-            <label>Your <b>{nameTitle}</b></label>
+            <label className="text-sm">Your <b>{nameTitle}</b></label>
             <input  className="outline-none cursor-auto text-black w-full border-2 border-gray-100 rounded-xl p-4 mt-1 bg-gray-100 text-xs font-medium" type={type} value={value} readOnly />
         </>
 
