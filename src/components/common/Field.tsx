dd4ead4a49b1e7fbb6c6fb39fb8b3bb7708dc77e
gsousaaa <glaucoezequiel94@gmail.com>
2024-05-20 12:19:51 -0300

    Alterando estilização do componente Field

--- src/components/common/Field.tsx
+++ src/components/common/Field.tsx
@@ -10,7 +10,7 @@ export const Field: React.FC<FieldProps> = ({ type, value, nameTitle }) => {
     return (
         <>
             <label>Your <b>{nameTitle}</b></label>
-            <input  className="outline-none cursor-auto bg-slate-100 text-black w-full border-2 border-gray-100 rounded-xl p-4 mt-1 bg-gray-100 text-sm font-medium" type={type} value={value} readOnly />
+            <input  className="outline-none cursor-auto text-black w-full border-2 border-gray-100 rounded-xl p-4 mt-1 bg-gray-100 text-xs font-medium" type={type} value={value} readOnly />
         </>
 
     )
